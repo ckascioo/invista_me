a91d89a7b3504cbb68103cdbcc3287872fe4f8d8
Cassio Campos <68764753+ckascioo@users.noreply.github.com>
2023-11-04 12:13:41 -0300

    teste

--- projeto_invista_me/projeto_invista_me/settings.py
+++ projeto_invista_me/projeto_invista_me/settings.py
@@ -79,8 +79,12 @@ WSGI_APPLICATION = 'projeto_invista_me.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'postgres', 
+        'USER': 'postgres', 
+        'PASSWORD': 'E0U9zBhUb2THQjbk',
+        'HOST': 'db.pskznorvvxnjjqguyozk.supabase.co', 
+        'PORT': '5432'
     }
 }
 
